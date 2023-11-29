782ac2e973bb8954cc43e70b9e0a6615b5e1fcca
nkatzenberger <80856925+nkatzenberger@users.noreply.github.com>
2023-11-29 16:39:20 -0500

    Updated Vertex

---
+++ Vertex.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Vertex {
+    public:
+        double x;
+        double y;
+        double z;
+        void setX;
+        void setY;
+        void setZ;
+        double getX;
+        double getY;
+        double getZ;
+
+void setX(double inx){
+    x = inx;
+}
+void setY(double iny){
+    y = iny;
+}
+void setZ(double inz){
+    z = inz;
+}
+double getX(){
+    return x;
+}
+double getY(){
+    return y;
+}
+double getZ(){
+    return z;
+}
+};
+int main(){
+Vertex newVertex;
+}
