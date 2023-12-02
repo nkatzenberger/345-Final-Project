4877f4e2bc62d32ffbf2d3086247c2a10872ed0f
nkatzenberger <penguindude1016@gmail.com>
2023-12-02 16:03:02 -0500

    fixed

--- UserInput.cpp
+++ UserInput.cpp
@@ -1,48 +1,54 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
+#include <list>
+#include<vector>
 #include "ref.h"
 #include <map>
 using namespace std;
 class UserInput {
 public:
-int [] colordefine(string color);
-int [] convertList(string input);
+double *colordefine(string color);
+double * convertList(string input);
 };
-int [] convertList(string input){
-    dobule xyz[3];
+double * convertList(string input){
+    double xyz[3];
     int count =0;
     string number = ""; 
      for (int x = 0; x<input.size(); x++){
-            if ((input[x] == ' ' || intput[x] == ",")&& number.size()>0){
+            if ((input[x] == ' ' || input[x] == ',')&& number.size()>0){
                 xyz[count] = stod(number);
                 number = ""; count ++;
             }
             else{
-                number = number + x;
+                number = number + input[x];
             }
         }
-        return(xyz);
+        double * pointer = xyz;
+        return(pointer);
 }
-int [] colordefine(string color){
-    map<string, int[3]>colorMap{{"red", [255, 0 ,0]},
-     {"orange", [255, 128, 0]},
-     {"yellow", [255, 255, 0]},
-     { "light green", [128, 255, 0]},
-     {"green", [0, 255, 0]},
-     {"green blue", [0, 255, 128]},
-     {"cyan", [0, 255, 255]},
-     {"light blue", [0, 128,255]},
-     {"blue", [0, 0, 255]},
-     {"purple", [127, 0, 255]},
-     {"magenta", [255, 0, 255]},
-     {"pink", [255, 0, 127]},
-     {"gray", [128, 128, 128]},
-     {"black", [0, 0, 0]},
-     {"white", [255, 255, 255]}};
+double * colordefine(string color){
+    map<string, vector<double>> colorMap = 
+    {   {"red", {255, 0 ,0}},
+     {"orange", {255, 128, 0}},
+     {"yellow", {255, 255, 0}},
+     { "light green", {128, 255, 0}},
+     {"green", {0, 255, 0}},
+     {"green blue", {0, 255, 128}},
+     {"cyan", {0, 255, 255}},
+     {"light blue", {0, 128,255}},
+     {"blue", {0, 0, 255}},
+     {"purple", {127, 0, 255}},
+     {"magenta", {255, 0, 255}},
+     {"pink", {255, 0, 127}},
+     {"gray", {128, 128, 128}},
+     {"black", {0, 0, 0}},
+     {"white", {255, 255, 255}}};
      if(colorMap.find(color)!=colorMap.end()){
-        return(colorMap[color]);
+       double arr [3];
+        copy(colorMap[color].begin(),colorMap[color].end(),arr);
+        double * pointer = arr;
+        return(pointer);
      }
      else{
         return(convertList(color));
@@ -51,19 +57,19 @@ int [] colordefine(string color){
 
 int main() {
     string yn = "yes";
-    list<Vertex> Vertices;
+    list<Vertex> Verticies;
     list<Face> Faces;
-    fcount = 1;
+  int fcount = 1;
     while(yn != "stop"){
         string input;
         string color;
-        cout<< "Enter Coordinates for the x, y, and z axis of vertex number " + Verticies.size()+1 + " of face number" + fcount + "in a comma or space separated list";
+        cout<< "Enter Coordinates for the x, y, and z axis of vertex number " , (Verticies.size()+1) , " of face number" , fcount , "in a comma or space separated list";
         cin >> input;
-        double coordinates[] =  convertList(input);
-        if(Verticies.size+1 > 3){
-            cout<< "Enter color of face. Either in RGB format or by name: "
+        double coordinates[3] = convertList(input);
+        if(Verticies.size()+1 > 3){
+            cout<< "Enter color of face. Either in RGB format or by name: ";
             cin >> color;
-            Face F(Verticies[0], Verticies[1], Verticies[2])
+            Face F(Verticies[0], Verticies[1], Verticies[2]);
             Faces.push_back(F);
             Verticies.clear();
             fcount++;
