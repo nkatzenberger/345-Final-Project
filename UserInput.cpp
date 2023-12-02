75c5e02e21cb910cfb593cb523d2a116f1185a6b
nkatzenberger <80856925+nkatzenberger@users.noreply.github.com>
2023-12-02 14:32:13 -0500

    update

--- UserInput.cpp
+++ UserInput.cpp
@@ -3,11 +3,51 @@
 #include <string>
 #include <vector>
 #include "ref.h"
+#include <map>
 using namespace std;
 class UserInput {
 public:
-
+int [] colordefine(string color);
+int [] convertList(string input);
 };
+int [] convertList(string input){
+    dobule xyz[3];
+    int count =0;
+    string number = ""; 
+     for (int x = 0; x<input.size(); x++){
+            if ((input[x] == ' ' || intput[x] == ",")&& number.size()>0){
+                xyz[count] = stod(number);
+                number = ""; count ++;
+            }
+            else{
+                number = number + x;
+            }
+        }
+        return(xyz);
+}
+int [] colordefine(string color){
+    map<string, int[3]>colorMap{{"red", [255, 0 ,0]},
+     {"orange", [255, 128, 0]},
+     {"yellow", [255, 255, 0]},
+     { "light green", [128, 255, 0]},
+     {"green", [0, 255, 0]},
+     {"green blue", [0, 255, 128]},
+     {"cyan", [0, 255, 255]},
+     {"light blue", [0, 128,255]},
+     {"blue", [0, 0, 255]},
+     {"purple", [127, 0, 255]},
+     {"magenta", [255, 0, 255]},
+     {"pink", [255, 0, 127]},
+     {"gray", [128, 128, 128]},
+     {"black", [0, 0, 0]},
+     {"white", [255, 255, 255]}};
+     if(colorMap.find(color)!=colorMap.end()){
+        return(colorMap[color]);
+     }
+     else{
+        return(convertList(color));
+     }
+}
 
 int main() {
     string yn = "yes";
@@ -16,31 +56,19 @@ int main() {
     fcount = 1;
     while(yn != "stop"){
         string input;
+        string color;
         cout<< "Enter Coordinates for the x, y, and z axis of vertex number " + Verticies.size()+1 + " of face number" + fcount + "in a comma or space separated list";
-        double  xyz [3]; 
         cin >> input;
-        string number;
-        int count =0;
-        for (int x = 0; x<input.size(); x++){
-            if (input[x] == ' ' || intput[x] == ","){
-                xyz[count] = stod(number);
-                number = ""; count ++;
-            }
-            else if (input.substr(x,2) = ", "){
-                 xyz[count] = stod(number);
-                number = ""; count +=2;
-            }
-            else{
-                number = number + x;
-            }
-        }
+        double coordinates[] =  convertList(input);
         if(Verticies.size+1 > 3){
+            cout<< "Enter color of face. Either in RGB format or by name: "
+            cin >> color;
             Face F(Verticies[0], Verticies[1], Verticies[2])
             Verticies.clear();
             fcount++;
         }
         else{
-         Vertex V(xyz[0], xyz[1], xyz[2]);
+         Vertex V(coordinates[0], coordinates[1], coordinates[2]);
         Vertices.push_back(V);
         cout<< "Enter stop to stop entering values";
         cin >> yn;   
@@ -72,5 +100,6 @@ int main() {
 
 	Model model1 = Model(faces);
  list<Faces> nan;
+
 
 }
