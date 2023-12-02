dd1a0de6d88b84ee808db1dfe17efae874bc941d
nkatzenberger <80856925+nkatzenberger@users.noreply.github.com>
2023-12-02 15:10:02 -0500

    Update UserInput.cpp
    Replaced previous code

--- UserInput.cpp
+++ UserInput.cpp
@@ -4,50 +4,73 @@ using namespace std;
 
 class UserInput {
 public:
-
+int [] colordefine(string color);
+int [] convertList(string input);
 };
+int [] convertList(string input){
+    dobule xyz[3];
+    int count =0;
+    string number = ""; 
+     for (int x = 0; x<input.size(); x++){
+            if ((input[x] == ' ' || intput[x] == ",")&& number.size()>0){
+                xyz[count] = stod(number);
+                number = ""; count ++;
+            }
+            else{
+                number = number + x;
+            }
+        }
+        return(xyz);
+}
+int [] colordefine(string color){
+    map<string, int[3]>colorMap{{"red", [255, 0 ,0]},
+     {"orange", [255, 128, 0]},
+     {"yellow", [255, 255, 0]},
+     { "light green", [128, 255, 0]},
+     {"green", [0, 255, 0]},
+     {"green blue", [0, 255, 128]},
+     {"cyan", [0, 255, 255]},
+     {"light blue", [0, 128,255]},
+     {"blue", [0, 0, 255]},
+     {"purple", [127, 0, 255]},
+     {"magenta", [255, 0, 255]},
+     {"pink", [255, 0, 127]},
+     {"gray", [128, 128, 128]},
+     {"black", [0, 0, 0]},
+     {"white", [255, 255, 255]}};
+     if(colorMap.find(color)!=colorMap.end()){
+        return(colorMap[color]);
+     }
+     else{
+        return(convertList(color));
+     }
+}
 
 int main() {
-    bool loop = true;
-    string input = "";
-    list<Face> faces;
-    while (loop) {
-        cout << "would you like to add a face? (y or n)";
+    string yn = "yes";
+    list<Vertex> Vertices;
+    list<Face> Faces;
+    fcount = 1;
+    while(yn != "stop"){
+        string input;
+        string color;
+        cout<< "Enter Coordinates for the x, y, and z axis of vertex number " + Verticies.size()+1 + " of face number" + fcount + "in a comma or space separated list";
         cin >> input;
-        if (input == "n") {
-            loop = false;
+        double coordinates[] =  convertList(input);
+        if(Verticies.size+1 > 3){
+            cout<< "Enter color of face. Either in RGB format or by name: "
+            cin >> color;
+            Face F(Verticies[0], Verticies[1], Verticies[2])
+            Faces.push_back(F);
+            Verticies.clear();
+            fcount++;
         }
-        else {
-            cout << "PLease enter coordinates for 3 vertices of the face";
-                cout << "vertex 1";
-                cout << "x position";
-                double x;
-                cin >> x;
-                cout << "y position";
-                double y;
-                cin >> y;
-                cout << "z position";
-                double z;
-                cin >> z;
-                Vertex v1(x, y, z);
-
-                cout << "vertex 2";
-                cout << "x position";
-                cin >> x;
-                cout << "y position";
-                cin >> y;
-                cout << "z position";
-                cin >> z;
-                Vertex v2(x, y, z);
-
-                cout << "vertex 3";
-                cout << "x position";
-                cin >> x;
-                cout << "y position";
-                cin >> y;
-                cout << "z position";
-                cin >> z;
-                Vertex v3(x, y, z);
+        else{
+         Vertex V(coordinates[0], coordinates[1], coordinates[2]);
+        Vertices.push_back(V);
+        cout<< "Enter stop to stop entering values";
+        cin >> yn;   
+    }
 
             cout << "enter 3 values for the RGB values";
                 int rgb[3];
