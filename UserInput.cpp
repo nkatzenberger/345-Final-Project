2beea8f095f04b8bd3ce6a3209bc478a3a947331
bpenman <bpenman@stevens.edu>
2023-12-02 14:41:00 -0500

    Model Object Created Successfully for first time

--- UserInput.cpp
+++ UserInput.cpp
@@ -1,61 +1,64 @@
-
-#include <iostream>
-#include <string>
-#include <vector>
 #include "ref.h"
+#include <vector>
 using namespace std;
+
 class UserInput {
 public:
 
 };
 
 int main() {
-    string yn = "yes";
-    vector<Vertex> Vertices;
-    while(yn == "yes"){
-        string input;
-        cout<< "Enter Coordinates for the x axis of the Vertex";
-        double x;
+    bool loop = true;
+    string input = "";
+    list<Face> faces;
+    while (loop) {
+        cout << "would you like to add a face? (y or n)";
         cin >> input;
-        x = stod(input);
-        cout<< "Enter Coordinates for the y axis fo the Vertex";
-        cin >> input;
-        double y;
-        y = stod(input);
-        cout << "Enter coordinates for the z axis of the Vertex";
-        cin >> input;
-        double z;
-        z = stod(input);
-        cout<< "Enter stop to stop putting inputs in";
-        cin >> yn;
-        Vertex V(x, y, z);
-        Vertices.push_back(V);
+        if (input == "n") {
+            loop = false;
+        }
+        else {
+            cout << "PLease enter coordinates for 3 vertices of the face";
+                cout << "vertex 1";
+                cout << "x position";
+                double x;
+                cin >> x;
+                cout << "y position";
+                double y;
+                cin >> y;
+                cout << "z position";
+                double z;
+                cin >> z;
+                Vertex v1(x, y, z);
+
+                cout << "vertex 2";
+                cout << "x position";
+                cin >> x;
+                cout << "y position";
+                cin >> y;
+                cout << "z position";
+                cin >> z;
+                Vertex v2(x, y, z);
+
+                cout << "vertex 3";
+                cout << "x position";
+                cin >> x;
+                cout << "y position";
+                cin >> y;
+                cout << "z position";
+                cin >> z;
+                Vertex v3(x, y, z);
+
+            cout << "enter 3 values for the RGB values";
+                int rgb[3];
+                cin >> rgb[0];
+                cin >> rgb[1];
+                cin >> rgb[2];
+
+                Face face(v1, v2, v3, rgb);
+                faces.push_back(face);
+        }
     }
-	//ask user, how many faces?
-    cout << "How many faces?";
-    int faces;
-    cin >> faces;
-	//const int number = 1;
-	vector<Face> vectorFaces;
-    int defaultColor[3] = {1,1,1}  
-    for(int i =0; i<= faces-3; i++){
-        Face f(Vertices[i], Vertices[i+1], Vertices[i+2], defaultColor);
-        vectorFaces.push_back(f);
-    }
 
-	//change aspects of faces
-	vectorFaces[0].V1.x = 1;
-	vectorFaces[0].V1.y = 1;
-	vectorFaces[0].V1.z = 1;
-
-	vectorFaces[0].V2.x = 0;
-	vectorFaces[0].V2.y = 2;
-	vectorFaces[0].V2.z = -1;
-	//leave V3 as 0,0,0
-
-	faces[0].RGB[0] = 255; //makes red tri
-
-	Model model1 = Model(faces);
- list<Faces> nan;
-
+    Model model(faces);
 }
