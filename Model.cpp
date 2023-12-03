274e1cfec1cd3b909f05dca316dfabc77edab031
nkatzenberger <penguindude1016@gmail.com>
2023-12-02 19:08:58 -0500

    Model  + toString()

--- Model.cpp
+++ Model.cpp
@@ -1,4 +1,5 @@
 #include "ref.h"
+#include <list>
 class Model {
 public:
 
@@ -7,6 +8,17 @@ public:
 	Model(list<Face> inputFaces) {
 			allFaces = inputFaces;
 	}
+	void toString() {
+        cout << "There are " << allFaces.size() << " faces";
+        int count = 1;
+        for (const Face& face : allFaces) {
+            cout << "Face " << count <<":" << endl;
+            cout << "  Vertex 1: (" << face.V1.x << ", " << face.V1.y << ", " << face.V1.z << ")" << std::endl;
+            cout << "  Vertex 2: (" << face.V2.x << ", " << face.V2.y << ", " << face.V2.z << ")" << std::endl;
+            cout << "  Vertex 3: (" << face.V3.x << ", " << face.V3.y << ", " << face.V3.z << ")" << std::endl;
+            count++;
+        }
+    }
 	/*void delete() {
 		for (Face allFaces[]: dd){
 		dd = null;
