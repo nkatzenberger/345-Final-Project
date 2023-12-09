5d0c4cb34e8f4317c751d99d0301cb5e7b301822
nkatzenberger <penguindude1016@gmail.com>
2023-12-09 15:55:23 -0500

    Test to see if things are pushing right

--- Renderer.cpp
+++ Renderer.cpp
@@ -1,1 +1,13 @@
+#include <GLAD/glad.h> 
 #include <OpenGL/glfw3.h>
+//test
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  
+    return 0;
+}
