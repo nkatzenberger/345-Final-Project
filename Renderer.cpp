467674d4d80b0ae9ce8305da0506bd8eb47ae8d7
nkatzenberger <penguindude1016@gmail.com>
2023-12-10 15:52:03 -0500

    Opengl files and glad files should be accessed through system libraries, NOT like this

--- Renderer.cpp
+++ Renderer.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <GLAD/glad.h>
-#include <OpenGL/glfw3.h>
+//#include <GLAD/glad.h>
+//#include <GLFW/glfw3.h>
 
 /*class Renderer {
     public:
-    }; */
+    }; 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 int main()
 {
@@ -36,5 +36,43 @@ void framebuffer_size_callback(GLFWwindow * window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
-
+glfw3.lib
+opengl32.lib
+user32.lib
+gdi32.lib
+shell32.lib
+*/
 //test
+#include <GLFW/glfw3.h>
+#include <windows.h>
+#include <stdio.h>
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
